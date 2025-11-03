3512c8bf24229c462019176dff01f36620f56d69
Paritosh Kailas <paritoshk2605@gmail.com>
2025-11-03 22:13:38 +0530

    addded some more edge cases

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator_keys')
 
@@ -84,96 +83,77 @@ if(!action){
 
 }
 if(action === 'calculate'){
-    const first_value = calculator.dataset
+    const first_value = calculator.dataset.first_value
+    const operator = calculator.dataset.operator
+    const second_value = displayedNum
 }
 
-=======
-const calculator = document.querySelector('.calculator')
-const keys = calculator.querySelector('.calculator_keys')
-
-keys.addEventListener('click', e => {
-    if(e.target.matches('button')){
-        const key = e.target
-        const action = key.dataset.action
-
+function  calculate(n1,n2,operator,result){
+    if(operator === 'add'){
+        result = parseFloat(n1) + parseFloat(n2)
     }
-})
-
-
-if(!action){
-    console.log('number key!')
-}
-if(
-    action === 'add' || 
-    action === 'subtract' ||
-    action === 'multiply'|| 
-    action === 'divide' 
-){
-    console.log('operator key')
-}
-if(
-    action === 'decimal'
-){
-    console.log('decimal key')
-}
-if(action === 'clear'){
-    console.log('AC key!')
-}
-if(action === 'calculate'){
-    console.log('equal-key!')
-}
-
-const display = document.querySelector('.calculator_display')
-keys.addEventListener('click', e => {
-    if(e.target.matches(button)){
-        const key = e.target
-        const action = key.dataset.action
-        const keycontent = key.textcontent
-        const displayedNum = display.textContent
+    else if(operator === 'subtract'){
+        result = parseFloat(n1) - parseFloat(n2)
     }
-
-})
-if(!action){
-    if(displayedNum === '0'){
-        display.textContent = keycontent
-    }else{
-        display.textContent = displayedNum + keycontent
+    else if(operator === 'multiply'){
+        result = parseFloat(n1) * parseFloat(n2)
     }
+    else if(operator === 'divide'){
+        result = parseFloat(n1) / parseFloat(n2)
+    }
+    return result
 }
-if(action === 'decimal'){
+if(!displayedNum.includes('.')){
     display.textContent = displayedNum + '.'
-
 }
 if( action === 'add' ||
     action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
+    action === 'divide' ||
+    action === 'multiply'
 ){
-    keys.classList.add("is-depressed")
-    calculator.datset.previouskeytype = 'operator'
-}
-keys.addEventListener('click',e =>{
-    if(e.target.matches('button')){
-        const key = e.target
+    const first_value = calculator.dataset.first_value
+    const operator = calculator.dataset.operator
+    const second_value = displayedNum
 
-        Array.from(key.parentNode.children)
-        .forEach(k => k.classList.remove('is-depressed'))
-    }
+    if(first_value && operator){
+    display.textContent = calculate(first_value,operator,second_value)
 }
 
-)
-if(!action){
-    const previouskeytype = calculator.dataset.previouskeytype
-    if(displayedNum === '0' || previouskeytype === 'operator'){
-        display.textContent = keycontent
-    }
-    else{
-        display.textContent = displayedNum + keycontent
-    }
+key.classList.add('is-depressed')
+calculator.dataset.previousKeyType = 'operator' 
+calculator.dataset.firstValue = displayedNum
+
+calculator.dataset.operator = action
 
 }
-if(action === 'calculate'){
-    const first_value = calculator.dataset
+if(first_value && 
+   operator &&
+   previousKeyType != 'operator'
+){
+    display.textContent = calculate(first_value,operator,second_value)
+
+    const calc_value = calculate(first_value,operator,second_value)
+    display.textContent = calc_value
+    calculator.database.first_value = calc_value
+}
+else{
+    calculator.database.first_value = displayedNum
+}
+key.classList.add('is-depressed')
+calculator.dataset.previousKeyType = 'operator'
+calculator.dataset.operator = action
+
+if (action === 'calculate') {
+  const firstValue = calculator.dataset.firstValue
+  const operator = calculator.dataset.operator
+  const secondValue = displayedNum
+
+if (firstValue) {
+    display.textContent = calculate(firstValue, operator, secondValue)
+  }
+
+calculator.dataset.previousKeyType = 'calculate'
 }
 
->>>>>>> 34d6a0d47862941b0d00a7876b8f100f4f35f879
+
+
